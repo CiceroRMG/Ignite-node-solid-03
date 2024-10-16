fd017e8f37c8ebdd09a116015ee72e94fc7ffe28
Cicero Gomes <cicero.ufsc@gmail.com>
2024-10-16 16:40:02 -0300

    Implementa testes unitarios para o useCase de cadastro

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,10 +1,16 @@
+import { User } from '@prisma/client'
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
 }
 
 // SOLID
@@ -18,19 +24,25 @@ interface RegisterUseCaseRequest {
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 4)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
-      throw new Error('E-mail already existis.')
+      throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
