404f3d62d33db58f982fac6f4d11ca6f85bcd8f7
Cicero Gomes <cicero.ufsc@gmail.com>
2024-10-17 14:04:26 -0300

    implementa o use case de autenticação

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,7 +1,7 @@
 import { User } from '@prisma/client'
 import { UsersRepository } from '@/repositories/users-repository'
-import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import bcrypt from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -29,7 +29,7 @@ export class RegisterUseCase {
     email,
     password,
   }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
-    const password_hash = await hash(password, 4)
+    const password_hash = await bcrypt.hash(password, 4)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
