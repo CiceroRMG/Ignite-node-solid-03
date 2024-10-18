5d3bf84d2df24d59b2212c4d211ba294fc8e6e78
Cicero Gomes <cicero.ufsc@gmail.com>
2024-10-18 15:37:50 -0300

    cria repositorio de gyms para usar no checkIn useCase

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -3,6 +3,10 @@ import { Prisma } from '@prisma/client'
 import { CheckInsRepository } from '../check-ins-repository'
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
+  //   async findByUserIdOnDate(userId: string, date: Date) {
+  //     throw new Error('Method not implemented.')
+  //   }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = await prisma.checkIn.create({
       data,
