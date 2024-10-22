9176eb10b92fb4b82ed0b75bea98a5284ec7c267
Cicero Gomes <cicero.ufsc@gmail.com>
2024-10-22 17:10:00 -0300

    cria useCase que procura as academias por busca e seus testes

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,24 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsCaseResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsCaseResponse> {
+    const gyms = await this.gymsRepository.findMany(query, page)
+
+    return { gyms }
+  }
+}
