e221915ad10f43211e5a0f1f493602cc96d68b82
Cicero Gomes <cicero.ufsc@gmail.com>
2024-10-24 15:57:16 -0300

    cria todos os controllers das rotas de checkIn

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -8,7 +8,7 @@ export async function search(request: FastifyRequest, reply: FastifyReply) {
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { page, query } = searchGymBodySchema.parse(request.body)
+  const { page, query } = searchGymBodySchema.parse(request.query)
 
   const searchGymUseCase = makeSearchGymsUseCase()
 
